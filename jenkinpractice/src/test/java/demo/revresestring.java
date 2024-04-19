c5a9f7d335d8df00deec9ad1a65ffddb0131f32a
satya5432 <syadav962@gmail.com>
2024-04-19 18:23:23 +0530

    create new class

---
+++ jenkinpractice/src/test/java/demo/revresestring.java
@@ -0,0 +1,14 @@
+package demo;
+
+public class revresestring {
+
+	public static void main(String[] args) {
+		String s="Welcome to java";
+		String rev="";
+		for(int i=s.length()-1;i>=0;i--) {
+			rev=rev+s.charAt(i);
+		}
+		System.out.println(rev);
+	}
+
+}
